c2ee3cf68e7c150deb4fd589cffc6e6b71ccf1a7
Rahul Nayak <rahnayak@publicisgroupe.net>
2022-05-11 11:58:28 +0530

    added shop component signin component nested routing using <outlet/> along with setting up the firebase

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,4 +1,5 @@
 import Directory from "../../components/directory/directory.component";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
   const categories = [
@@ -29,7 +30,11 @@ const Home = () => {
     },
   ];
 
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Directory categories={categories} /> <Outlet />
+    </div>
+  );
 };
 
 export default Home;
